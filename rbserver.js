4654cf3734233b35d9aafb9e43ce99c3fd2a4067
mick <mccoyc@cat.pdx.edu>
2018-08-12 14:14:31 -0700

    add names to all input fields for parsing

--- rbserver.js
+++ rbserver.js
@@ -12,7 +12,13 @@ rbs.get('/', (req, res) => {
 });
 
 rbs.post('/interactive', urlencodedParser, (req, res) => {
-  res.send(req.body.firstName + ' ' + req.body.lastName);
+  res.write('<header class="header bg-info">');
+  res.write('<div class="jumbotron bg-info">');
+  res.write('<h1 class="display-3 text-white">' + req.body.firstName + ' ' + req.body.lastName + '</h1>');
+  res.write('<script><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.min.css"/><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>');
+  res.write('</header>');
+  res.write(req.body.email);
+  res.end()
 });
 
 rbs.listen(8080);
