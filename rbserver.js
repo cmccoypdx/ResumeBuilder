c7edf774ea530477a2d499b1beabb767b636a271
mick <mccoyc@cat.pdx.edu>
2018-08-12 11:23:43 -0700

    add basic form parsing and response with body-parser

--- rbserver.js
+++ rbserver.js
@@ -1,9 +1,18 @@
+'use strict';
+
 var express = require('express');
 var rbs = express();
 var path = require('path');
+var parser = require('body-parser');
+var urlencodedParser = parser.urlencoded({ extended: false });
 
 rbs.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/form.html'));
+  res.status(200);
+  res.sendFile(path.join(__dirname + '/form.html'));
+});
+
+rbs.post('/interactive', urlencodedParser, (req, res) => {
+  res.send(req.body.firstName + ' ' + req.body.lastName);
 });
 
 rbs.listen(8080);
